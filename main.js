ed6123fae76007facfb939410e8110a43d4a8b2f
ls1w <michelle_j.b@hotmail.com>
2023-11-27 10:49:20 -0300

    feito

--- main.js
+++ main.js
@@ -1,42 +1,43 @@
-'user strict'
+'use strict'
 
 const botaoExibir = document.getElementById('exibir')
-const botaoVendas100 = document.getElementById(vendas100)
+const botaoVendas100 = document.getElementById('vendas100')
 const botaoBonus = document.getElementById('bonus')
-const numeros = [80, 50, 23, 7, 100, 254, 30, 500, 1500]
 
-function criarCard (data, container){
+const numeros= [80, 50, 23, 7, 100, 254, 30, 500, 1500]
+
+function criarCard (data) {
     const container = document.getElementById('data-container')
     const card = document.createElement('div')
     card.textContent = data
     container.appendChild(card)
+
 }
 
-function limparContainer() {
+function limparContainer () {
     const container = document.getElementById('data-container')
-    container.textContent = ''
+    container.textContent = ' '
 }
 
-function exibir(){
+function exibir () {
     limparContainer()
-    numeros.forEach (criarCard)
+    numeros.forEach(criarCard)
 }
 
-function vendas100 {
-    limparContainer ()
-    const numeros100 = numeros.filter( numero => numero < 100 )
-    numeros.forEach(criarCard)
+function vendas100 () {
+    limparContainer()
+    const numeros100 = numeros.filter( numero=> numero > 100 )
+    numeros100.forEach(criarCard)
 }
 
 function vendasBonus () {
     limparContainer()
-    const numerosBonus = numeros.map (numero => numero + 10)
+    const numerosBonus = numeros.map ( numero=> numero + 10 )
     numerosBonus.forEach(criarCard)
-}
+
+}    
 
 botaoExibir.addEventListener('click', exibir)
 botaoVendas100.addEventListener('click', vendas100)
-botaoBonus.addEventListener('click', vendasBonus )
-
-
+botaoBonus.addEventListener('click', vendasBonus)
 
